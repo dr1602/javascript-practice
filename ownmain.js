96afc4be433cf6aa967071031e43823497f283e2
dr1602 <109833006+dr1602@users.noreply.github.com>
2022-11-16 11:22:58 -0600

    aside
    own version of aside menu for product details with html, css and js

---
+++ ownmain.js
@@ -0,0 +1,134 @@
+// vamos a funcionar los distintos elementos
+
+const navEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+
+const menuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
+
+const cardsContainer = document.querySelector(".cards-container");
+
+const productDescriptionClose = document.querySelector(".product-description-close");
+const addCartButton = document.querySelector(".add-to-cart-button");
+
+navEmail.addEventListener("click",toggleDesktopMenu);
+menuIcon.addEventListener("click",toggleMobileMenu);
+shoppingCart.addEventListener("click",toggleProductDetail);
+
+productDescriptionClose.addEventListener("click",toggleProdClose);
+addCartButton.addEventListener("click",toggleProdClose);
+
+//enable & disable desktop menu while also desibling the other menus
+function toggleDesktopMenu() {
+    console.log("click");
+    desktopMenu.classList.toggle("inactive");
+    productDetail.classList.add("inactive");
+}
+
+//enable & disable mobile menu while also desibling the other menus
+function toggleMobileMenu() {
+    console.log("click");
+    mobileMenu.classList.toggle("inactive");
+    productDetail.classList.add("inactive");
+}
+
+//enable & disable product detail while also desibling the other menus
+function toggleProductDetail() {
+    console.log("click");
+    productDetail.classList.toggle("inactive");
+    desktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+}
+
+console.log("JS funcionando");
+
+// allows the input of new products and its characteristics
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name: 'Patalla',
+    price: 220,
+    image: "https://p1.pxfuel.com/preview/104/721/245/vintage-tv-tv-old-transistor.jpg",
+});
+
+productList.push({
+    name: 'Computadora',
+    price: 300,
+    image: "https://p0.pxfuel.com/preview/461/248/853/laptop-computer-browser-business.jpg",
+});
+
+/*
+for (product of productList) {
+    console.log(product.name);
+}
+
+for (product in productList) {
+    console.log(product);
+}
+*/
+
+/*
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>
+*/
+
+function renderProducts(arr) {
+
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        // product ={name, price, image} -> product.image
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src',product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price + '.00';
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.append(productPrice, productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.append(productInfoDiv, productInfoFigure);
+    
+        productCard.append(productImg, productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+
+}
+
+// de esta forma podemos llamar los productos al apretar un botón en el html
+renderProducts(productList);
+
